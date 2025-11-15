86b0c81014f914be7476dad949d2930a103381e3
ajaymehta94 <ajaymehta0940@gmail.com>
2025-11-15 07:42:02 +0530

    Create Main.java with basic Java program
    Initial implementation of a Java program with variable declarations and basic arithmetic operations.

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main (String [] args ) {
+   //output 
+    System.out.print ("He'll World");
+    //next line 
+    System.out.println("Hello kitty");
+    // baxk /n
+    // write a pattern 
+    System.out.println("*/n**/n***");
+    // variables in java 
+    int a = 10;
+    int b = 20;
+    int age = 30;
+    string name = "tont stark";
+    // use of variables
+    int a = 40;
+    int b  = 80;
+    int sum = a + b;
+    System.out.println(sum);
+    int diff = b - a;
+    System.out.println(diff);
+    int mul = a * b;
+    System.out.println(mul);
+    // add scanner to  put variable to give user value
+    //in next page 📄 ~>
+  }
+}
