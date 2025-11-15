92e70d5c8002edd6baf938426229a6ad0367640d
ajaymehta94 <ajaymehta0940@gmail.com>
2025-11-15 08:24:59 +0530

    Correct syntax and improve user prompts in Main2
    Fix various syntax errors and improve prompts in Main2.java.

--- Main2.java
+++ Main2.java
@@ -4,25 +4,29 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     //input by user
-    Scanner Sc = new scanner(Syste.in);
+    Scanner Sc = new scanner(System.in);
+    System.out.println("Enter name");
     String name = Sc.nextline();
                     //nextInt();
                     //nextFleat();
     System.out.println(name);
 
-    // Variables "a" & "b" and print their Sum ?
-    int a = Sc.nextInt();
+    // Variables "a" & "b" and print their Sum up ?
+      System.out.prinln(" a = ");  
+    int= Sc.nextInt();
+    System.out.println(" b = ");
     int b = Sc.nextInt();
     int Sum = a + b;
     System.out.println(Sum);
 
     //CONDITION STATMENT IN JAVA
      // IF ELSE
+     System.out.println("Age = ");
     int age = Sc.nextInt();
     if (age > 18) {
       System.out.println("Adult");
     }
-    elelMain2
+    else {
       System.out.println("bacha");
     }
   }
