b811367093e622bda2f8e840bf9e70466bbb4afe
ajaymehta94 <ajaymehta0940@gmail.com>
2025-11-15 07:56:43 +0530

    Fix typo in else block and output message

---
+++ Main2.java
@@ -0,0 +1,29 @@
+//package com.tgdajay;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //input by user
+    Scanner Sc = new scanner(Syste.in);
+    String name = Sc.nextline();
+                    //nextInt();
+                    //nextFleat();
+    System.out.println(name);
+
+    // Variables "a" & "b" and print their Sum ?
+    int a = Sc.nextInt();
+    int b = Sc.nextInt();
+    int Sum = a + b;
+    System.out.println(Sum);
+
+    //CONDITION STATMENT IN JAVA
+     // IF ELSE
+    int age = Sc.nextInt();
+    if (age > 18) {
+      System.out.println("Adult");
+    }
+    elelMain2
+      System.out.println("bacha");
+    }
+  }
+}
